2169bbe64dd3c687a28f27471f2c99c18e88bd2c
Gabriela Trindade Pires - GPR <gabriela.pires@iteris.com.br>
2021-01-21 18:13:44 -0300

    Script SQL

---
+++ docs/scriptSQL.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE TODO;
+
+USE TODO;
+
+CREATE TABLE USERS(
+	id int identity,
+	username nvarchar(40) not null,
+	password nvarchar(40) not null,
+	constraint PK_USERS primary key (id)
+)
+
+
+
+CREATE TABLE TASKS(
+	id int identity,
+	user_id int not null,
+	name nvarchar(40) not null,
+	date datetime not null,
+	constraint PK_TASKS primary key (id),
+	constraint FK_USERID_REFERENCE_USERS foreign key (user_id) references "USERS" (id)
+)
+
+
+INSERT INTO USERS (username, password) VALUES
+('RHE', '1234'),
+('FAV', '4567'),
+('GPR', '8911')
+
+INSERT INTO TASKS (user_id, name, date) VALUES
+(1, 'Estudar Java POO', GETDATE()),
+(2, 'Estudar Angular', GETDATE()),
+(3, 'Estudar Autenticação', GETDATE())
+(1, 'Criptografia', GETDATE()),
+(1, 'Inglês', GETDATE()),
+(2, 'Reunião Diretoria', GETDATE())
+
+SELECT U.username, T.name, T.date FROM USERS U
+INNER JOIN TASKS T
+ON U.id = T.user_id
